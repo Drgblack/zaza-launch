1b5004be2ec12abc710d2b65ff6079ee3ce514f8
Drgblack <blackburn.greg@gmail.com>
2025-05-09 01:05:27 +0200

    Add files via upload

---
+++ Zaza_Web_GitHub_Deploy/src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDov8anmioSv-Rgptgdso1Hhc0NStWt0",
+  authDomain: "zaza-launch.firebaseapp.com",
+  projectId: "zaza-launch",
+  storageBucket: "zaza-launch.appspot.com",
+  messagingSenderId: "699651890444",
+  appId: "1:699651890444:web:f38f421fa5fd818afdad55"
+};
+
+export default firebaseConfig;
